0ed9dfdd11d235ce4c56d6302399c4d00adf6756
Heather Kirk <hskirk@gmail.com>
2019-04-15 03:52:47 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   
   get '/name' do 
-    @name = name 
+    @name = name(name => "Heather") 
     "My name is _"
     erb :'/name/index'
   end 
