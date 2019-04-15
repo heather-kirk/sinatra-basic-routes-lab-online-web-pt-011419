de42a21a79a64d74cf0891c3184f65d3479d3277
Heather Kirk <hskirk@gmail.com>
2019-04-15 03:53:40 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,18 +3,18 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   
   get '/name' do 
-    @name = name(name => "Heather") 
+
     "My name is _"
-    erb :'/name/index'
+    
   end 
   
   get '/hometown' do 
     "My hometown is _"
-    erb :'/hometown/index'
+   
   end 
   
   get '/favorite-song' do 
     "My favorite song is _"
-    erb :'/favorite-song/index'
+    
   end 
 end
